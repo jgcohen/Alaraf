1cca7ac5e8ab8d41b6cfad2372ff2dc400ddc8dc
jgcohen <75163282+jgcohen@users.noreply.github.com>
2022-01-23 19:50:37 +0100

    adding comments

--- src/Controller/JournalController.php
+++ src/Controller/JournalController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Comments;
 use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use DateTime;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
 
 class JournalController extends AbstractController
 {
 
     private $entityManager;
 
-    public function __construct (EntityManagerInterface $entityManager){
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
     }
 
@@ -27,5 +32,34 @@ class JournalController extends AbstractController
         return $this->render('journal/index.html.twig', [
             'posts' => $posts
         ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post")
+     */
+    public function post($id, Request $request): Response
+    {
+
+        $post = $this->entityManager->getRepository(Post::class)->find($id);
+        if ($post) {
+
+            $comment = new Comments;
+            $form = $this->createForm(CommentType::class, $comment);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $comment->setUser($this->getUser());
+                $comment->setCreatedAt(new DateTime('now'));
+                $comment->setPost($post);
+
+                $this->entityManager->persist($comment);
+                $this->entityManager->flush();
+            }
+            return $this->render('journal/post.html.twig', [
+                'post' => $post,
+                'comments' => $post->getComments(),
+                'form' => $form->createView(),
+            ]);
+        }
     }
 }
---
+++ src/Form/CommentType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comments;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content',TextareaType::class, [
+                'attr' => ['class' => 'tinymce'],
+                
+            ])
+            ->add('Comment', SubmitType::class, [
+                'attr' => ['class' => 'btn-info text-light mt-3'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comments::class,
+        ]);
+    }
+}
